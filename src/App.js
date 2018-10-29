6e3483ae9dd6f057923a55bb7c10d649033de630
annamrp <annamrp22@gmail.com>
2018-10-29 16:34:19 +0100

    no hace falta rutas a imagen en index.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Intro from './components/Intro';
 import Description from './components/Description';
